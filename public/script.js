2fb755541fb2633f60a946f6bdb39b94d737d431
Zsolt Alföldi <zsolt.alfoldi@icellmobilsoft.hu>
2022-01-21 13:03:23 +0100

    tsting

--- public/script.js
+++ public/script.js
@@ -1,6 +1,17 @@
-function request(url, method, body) {
+// import request from 'request'
+// var request = require('request');
+// import { request } from "request"
+// import { writeFileSync } from 'fs';
+// const request = require("request")
+
+// import { request } from '../node_modules/request/request.js';
+// import { request } from '../node_modules/request/request.js';
+// import * as request from '../node_modules/request/request.js';
+
+function _request(url, method, body) {
   return fetch(url, {
     method,
+    mode: 'no-cors',
     body: JSON.stringify(body),
     headers: {
       'Content-Type': 'application/json'
@@ -60,7 +71,9 @@ const app = new Vue({
       });
     },
     orderedMangas: function () {
-      return this.listOfManga.sort((a, b) => {
+      // have to use slice() method too, cuz sort will modify listOfManga object
+      // which is reactive because of Vue, so Vue will re-render the page twice ...
+      return this.listOfManga.slice().sort((a, b) => {
         return a.order - b.order;
       });
     },
@@ -68,7 +81,7 @@ const app = new Vue({
   methods: {
     testit: function (txt) {
       // e.preventDefault();
-      console.log(txt)
+      // console.log(txt)
     },
     modifyMangaPageNumber: function (manga, event) {
       if (event) event.preventDefault()
@@ -77,7 +90,7 @@ const app = new Vue({
 
       this.pageNumTimer[manga.id] = setTimeout(() => {
         this.error = null;
-        request('/api/manga/page/' + manga.id, 'PATCH', {
+        _request('/api/manga/page/' + manga.id, 'PATCH', {
           pageNum: manga.pageNum
         })
           .then(manga => {
@@ -99,7 +112,7 @@ const app = new Vue({
         this.errors.push('Manga url required.');
       }
 
-      request('/api/manga', 'POST', { name: this.mangaName, url: this.mangaUrl })
+      _request('/api/manga', 'POST', { name: this.mangaName, url: this.mangaUrl })
         .then(manga => this.listOfManga.push(manga))
         .catch(error => this.error = error);
 
@@ -121,7 +134,7 @@ const app = new Vue({
 
       this.adminTimer[manga.id] = setTimeout(() => {
         this.error = null;
-        request('/api/manga/admin/edit/' + manga.id, 'PATCH', {
+        _request('/api/manga/admin/edit/' + manga.id, 'PATCH', {
           name: manga.name,
           url: manga.url,
           order: manga.order,
@@ -137,7 +150,7 @@ const app = new Vue({
       if (event) event.preventDefault()
 
       this.error = null;
-      request('/api/manga/hide/' + manga.id, 'POST')
+      _request('/api/manga/hide/' + manga.id, 'POST')
         .then(() => {
           const index = this.listOfManga.indexOf(manga);
           this.listOfManga[index].hidden = !this.listOfManga[index].hidden
@@ -151,7 +164,7 @@ const app = new Vue({
 
       this.error = null;
       if (confirm("Do you really want to delete?")) {
-        request('/api/manga/delete/' + manga.id, 'DELETE')
+        _request('/api/manga/delete/' + manga.id, 'DELETE')
           .then(() => {
             this.listOfManga.splice(this.listOfManga.indexOf(manga), 1);
           })
@@ -187,7 +200,7 @@ const auth = new Vue({
         this.errors.push('password required.');
       }
 
-      request('/auth/login', 'POST',
+      _request('/auth/login', 'POST',
         { email: this.email, password: this.password })
         .then(() => {
           location.reload();
@@ -196,7 +209,7 @@ const auth = new Vue({
     },
     logout: function (event) {
       if (event) event.preventDefault()
-      request('/auth/logout', 'POST')
+      _request('/auth/logout', 'POST')
         .then(() => {
           location.reload();
         })
@@ -208,53 +221,195 @@ const auth = new Vue({
 })
 
 app.error = null;
-request('/api/mangas', 'GET')
-  .then((listOfManga) => {
+_request('/api/mangas', 'GET')
+  // .then((listOfManga) => {
+  .then(function (listOfManga) {
+    // .then(async function (listOfManga) {
+    for (let manga of listOfManga) {
+      manga.editedUrl = manga.url.replace('%%N%%', manga.pageNum)
+      manga.nextUrl = manga.url.replace('%%N%%', manga.pageNum + 1)
+      // let fef = await nextUrlCheck(manga.editedUrl)
+      // let fef = nextUrlCheck(manga.nextUrl)
+      // console.log(fef)
+    }
     app.listOfManga = listOfManga
     auth.isUserLoggedIn = true
   })
   .catch((error) => {
     app.error = error
     auth.isUserLoggedIn = false
-    request('/auth/cleanUpExpiredSessions', 'GET')
+    _request('/auth/cleanUpExpiredSessions', 'GET')
   });
-// request('https://w12.mangafreak.net/Read1_Onepunch_Man_157', 'GET').then(p => console.log(p)).catch((error) => {
-//   console.log(error)
-// });
 
-const headers = new Headers();
-headers.set('Access-Control-Allow-Origin', '*');
+// async function nextUrlCheck(url) {
+//   try {
+//     const resp = await fetch(
+//       url, {
+//       method: 'GET',
+//       redirect: 'error',
+//       mode: 'no-cors',
+//       headers: {
+//         'Content-Type': 'application/json',
+//       }
+//     });
+//     console.info(resp + " <xxx ");
+//     return true;
+//   } catch (err) {
+//     console.info(err + " <==== ");
+//     return false;
+//   }
+// }
 
-// const init = {
-//   method: HTTP_METHODS.GET,
-//   headers
+// _request('https://api.github.com/users/KrunalLathiya', 'GET')
+//   .then(function (resp) {
+//     console.info(resp + " <xxxxx ");
+//   })
+//   .catch(function (err) {
+//     console.info(err + " <==== ");
+//   });
+
+
+// async function firstFunction() {
+//   const response = await fetch('https://w12.mangafreak.net/Read1_Onepunch_Man_156', {
+//     headers: {
+//       // 'Content-Type': 'application/json',
+//       'Access-Control-Allow-Origin': '*',
+//       'Access-Control-Allow-Headers': '*',
+//       "Access-Control-Allow-Methods": '*',
+//     },
+//     // mode: 'same-origin',
+//   })
+//   console.log(response.status)
+//   const data = await response.text();
+//   console.log(data)
+//   console.log(response.status)
 // };
-// fetch('https://w12.mangafreak.net/Read1_Onepunch_Man_157', init).then(p => console.log(p))
-fetch(
-  'https://w12.mangafreak.net/Read1_Onepunch_Man_158', {
-  method: 'GET',
-  redirect: 'error',
-  mode: 'no-cors',
-  // body: JSON.stringify(''),
-  headers: {
-    'Content-Type': 'application/json',
-    // 'Access-Control-Allow-Origin': '*'
-  }
-})
-  .then(resp => console.log(resp))
-  .catch(function (err) {
-    console.info(err + " <==== ");
-  });
-// .catch(p => console.log(p))
+
+// firstFunction()
+
+// // fetch('https://api.github.com/users/KrunalLathiya')
+// await fetch('https://w12.mangafreak.net/Read1_Onepunch_Man_156', {
+//   mode: 'no-cors',
+// })
+//   .then(response => {
+//     try {
+//       console.log(response.status); // Will show you the status
+//       console.log("kekk")
+//       return response.text()
+//       return response.json()
+//     } catch (error) {
+//       console.log("feeeeeeee")
+//       return response
+//     }
+//   })
+//   .then(data => {
+//     console.log("reeeeeeeeeee")
+//     console.log(data) // Prints result from `response.json()` in getRequest
+//   })
+//   .catch(error => {
+//     console.error(error)
+//   })
+
 // fetch(
 //   'https://w12.mangafreak.net/Read1_Onepunch_Man_156', {
 //   method: 'GET',
+//   // redirect: 'error',
+//   redirect: 'manual',
 //   mode: 'no-cors',
 //   // body: JSON.stringify(''),
 //   headers: {
 //     'Content-Type': 'application/json',
-//     'Access-Control-Allow-Origin': '*'
+//     'Access-Control-Allow-Origin': '*',
+//     'Access-Control-Allow-Headers': '*'
 //   }
 // })
-// .then(p => console.log(p))
-// .catch(p => console.log(p))
+//   .then(function (resp) {
+//     console.info(resp + " <xxxxx ");
+//   })
+//   .catch(function (err) {
+//     console.info(err + " <==== ");
+//   });
+
+// nextUrlCheck("https://w12.mangafreak.net/Read1_Onepunch_Man_156")
+
+// const request = require('request');
+// requirejs(["request"], function (request) {
+//   // const headerEl = document.getElementById("header");
+//   // headerEl.textContent = lodash.upperCase("hello world");
+//   request('http://www.google.com', function (error, response, body) {
+//     console.error('error:', error); // Print the error if one occurred
+//     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//     console.log('body:', body); // Print the HTML for the Google homepage.
+//   });
+// });
+
+// request('http://www.google.com', function (error, response, body) {
+//   console.error('error:', error); // Print the error if one occurred
+//   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', body); // Print the HTML for the Google homepage.
+// });
+
+
+// fetch('http://www.google.com')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+// fetch('https://api.github.com/users/KrunalLathiya')
+// await fetch(
+//   // 'https://w12.mangafreak.net/Read1_Onepunch_Man_156', {
+//   'https://api.github.com/users/KrunalLathiya', {
+//   headers: {
+//     'Content-Type': 'text/html',
+//     //     'Access-Control-Allow-Origin': '*',
+//     //     'Access-Control-Allow-Headers': '*'
+//   },
+//   mode: 'no-cors',
+// })
+//   .then(response => response.json())
+//   .then(response => console.log(response.json()))
+//   .catch((error) => {
+//     console.log(error)
+//   });
+
+// await fetch(
+//   'https://w12.mangafreak.net/Read1_Onepunch_Man_157', {
+//   headers: {
+//     'Content-Type': 'text/html',
+//     //     'Access-Control-Allow-Origin': '*',
+//     //     'Access-Control-Allow-Headers': '*'
+//   },
+//   mode: 'no-cors',
+// })
+//   .then(response => console.log(response))
+//   .catch((error) => {
+//     console.log(error)
+//   });
+
+
+// fetch('https://w12.mangafreak.net/Read1_Onepunch_Man_156', {
+//   mode: 'no-cors',
+//   // redirect: 'manual',
+//   method: "GET",
+//   headers: {
+//     'Content-Type': 'application/json',
+//   //   "Access-Control-Allow-Origin": "*",
+//   //   "Access-Control-Allow-Credentials": "true",
+//   //   "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
+//   //   "Access-Control-Allow-Headers": "Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control",
+//   }
+// }).then(response => {
+//   // if (!response.ok) {
+//   //   let faf = Promise.reject({
+//   //     status: response.status,
+//   //     statusText: response.statusText,
+//   //     method: "GET",
+//   //     url: response.url
+//   //   });
+//   //   console.log(faf)
+//   //   return
+//   // }
+
+//   console.log(response)
+
+//   // return response.json().catch(() => { });
+// });
--- src/app/controllers/auth.controller.ts
+++ src/app/controllers/auth.controller.ts
@@ -2,6 +2,8 @@ import { Config, Context, createSession, dependency, Get, HttpResponseOK, HttpRe
 
 import { User } from '../entities';
 
+import * as cheerio from 'cheerio';
+
 const credentialsSchema = {
   additionalProperties: false,
   properties: {
@@ -70,8 +72,107 @@ export class AuthController {
 
   @Get('/cleanUpExpiredSessions')
   async cleanUpExpiredSessions(ctx: Context<any, Session>) {
-    const expirationTimeouts: {absolute:number, inactivity: number} = Config.get('settings.session.expirationTimeouts', 'any', { absolute: 604800, inactivity: 900 });
+    const expirationTimeouts: { absolute: number, inactivity: number } = Config.get('settings.session.expirationTimeouts', 'any', { absolute: 604800, inactivity: 900 });
     this.store.cleanUpExpiredSessions(expirationTimeouts.inactivity, expirationTimeouts.absolute)
+    return new HttpResponseOK();
+  }
+
+  @Get('/tst')
+  async tst(ctx: Context<any, Session>) {
+    const request = require('request');
+    // request('https://w12.mangafreak.net/Read1_Onepunch_Man_156', function (error, response, body) {
+    //   // console.error('error:', error); // Print the error if one occurred
+    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    //   // console.log('body:', body); // Print the HTML for the Google homepage.
+    // });
+
+
+
+    request('https://www.mangaread.org/manga/overgeared-2020-manhwa/chapter-111/',
+      {
+        method: 'GET',
+        followRedirect: false,
+        followAllRedirects: false
+      },
+      function (error, response, body) {
+        // console.error('error:', error); // Print the error if one occurred
+        console.log('111 statusCode:', response.statusCode); // Print the response status code if a response was received
+        // console.log('statusCode:', response); // Print the response status code if a response was received
+        // console.log('body:', body); // Print the HTML for the Google homepage.
+        // var parser = new DOMParser();
+        // var htmlDoc = parser.parseFromString(body, 'text/html');
+        // console.log(
+        //   // htmlDoc.querySelectorAll()
+        //   htmlDoc.title
+        // )
+        // const cheerio = require('cheerio');
+        const $ = cheerio.load(body);
+        console.log(
+          $("title").text(),
+          // $.html(),
+          // response
+        )
+      });
+
+
+
+    request('https://www.mangaread.org/manga/overgeared-2020-manhwa/chapter-115/',
+      {
+        method: 'GET',
+        followRedirect: false,
+        followAllRedirects: false
+      },
+      function (error, response, body) {
+        // console.error('error:', error); // Print the error if one occurred
+        console.log('1115 statusCode:', response.statusCode); // Print the response status code if a response was received
+        // console.log('statusCode:', response); // Print the response status code if a response was received
+        // console.log('body:', body); // Print the HTML for the Google homepage.
+        // const cheerio = require('cheerio');
+        const $ = cheerio.load(body);
+        console.log(
+          $("title").text()
+        )
+      });
+
+
+
+
+    // request('https://w12.mangafreak.net/Read1_Onepunch_Man_156',
+    request('https://www.mangaread.org/manga/the-beginning-after-the-end/chapter-130/',
+      {
+        method: 'GET',
+        followRedirect: false,
+        followAllRedirects: false
+      },
+      function (error, response, body) {
+        // console.error('error:', error); // Print the error if one occurred
+        console.log('ok statusCode:', response.statusCode); // Print the response status code if a response was received
+        // console.log('statusCode:', response); // Print the response status code if a response was received
+        // console.log('body:', body); // Print the HTML for the Google homepage.
+        const $ = cheerio.load(body);
+        console.log(
+          $("title").text()
+        )
+      });
+
+
+    // request('https://w12.mangafreak.net/Read1_Onepunch_Man_157',
+    request('https://www.mangaread.org/manga/the-beginning-after-the-end/chapter-128/',
+      {
+        method: 'GET',
+        followRedirect: false,
+        followAllRedirects: false
+      },
+      function (error, response, body) {
+        // console.error('error:', error); // Print the error if one occurred
+        console.log('mo statusCode:', response.statusCode); // Print the response status code if a response was received
+        // console.log('statusCode:', response); // Print the response status code if a response was received
+        // console.log('body:', body); // Print the HTML for the Google homepage.
+        const $ = cheerio.load(body);
+        console.log(
+          $("title").text()
+        )
+      });
     return new HttpResponseOK();
   }
 }
