6a0a3d0a2aca4d0cc652866874baa44e447a6a07
Zsolt Alföldi <zsolt.alfoldi@icellmobilsoft.hu>
2022-01-21 19:43:28 +0100

    .

--- src/app/controllers/auth.controller.ts
+++ src/app/controllers/auth.controller.ts
@@ -1,8 +1,12 @@
-import { Config, Context, createSession, dependency, Get, HttpResponseOK, HttpResponseRedirect, HttpResponseUnauthorized, Post, Session, Store, ValidateBody, verifyPassword } from '@foal/core';
+import { ApiInfo, Config, Context, createSession, dependency, Get, HttpResponseOK, HttpResponseRedirect, HttpResponseUnauthorized, Post, Session, Store, ValidateBody, verifyPassword } from '@foal/core';
 
 import { User } from '../entities';
 
 import * as cheerio from 'cheerio';
+import axios from 'axios';
+// import { axios } from 'axios';
+
+// const axios = require('axios');
 
 const credentialsSchema = {
   additionalProperties: false,
@@ -88,92 +92,174 @@ export class AuthController {
 
 
 
-    request('https://www.mangaread.org/manga/overgeared-2020-manhwa/chapter-111/',
-      {
-        method: 'GET',
-        followRedirect: false,
-        followAllRedirects: false
-      },
-      function (error, response, body) {
-        // console.error('error:', error); // Print the error if one occurred
-        console.log('111 statusCode:', response.statusCode); // Print the response status code if a response was received
-        // console.log('statusCode:', response); // Print the response status code if a response was received
-        // console.log('body:', body); // Print the HTML for the Google homepage.
-        // var parser = new DOMParser();
-        // var htmlDoc = parser.parseFromString(body, 'text/html');
-        // console.log(
-        //   // htmlDoc.querySelectorAll()
-        //   htmlDoc.title
-        // )
-        // const cheerio = require('cheerio');
-        const $ = cheerio.load(body);
-        console.log(
-          $("title").text(),
-          // $.html(),
-          // response
-        )
-      });
+    // request('https://www.mangaread.org/manga/overgeared-2020-manhwa/chapter-111/',
+    //   {
+    //     method: 'GET',
+    //     followRedirect: false,
+    //     followAllRedirects: false
+    //   },
+    //   function (error, response, body) {
+    //     // console.error('error:', error); // Print the error if one occurred
+    //     console.log('111 statusCode:', response.statusCode); // Print the response status code if a response was received
+    //     // console.log('statusCode:', response); // Print the response status code if a response was received
+    //     // console.log('body:', body); // Print the HTML for the Google homepage.
+    //     // var parser = new DOMParser();
+    //     // var htmlDoc = parser.parseFromString(body, 'text/html');
+    //     // console.log(
+    //     //   // htmlDoc.querySelectorAll()
+    //     //   htmlDoc.title
+    //     // )
+    //     // const cheerio = require('cheerio');
+    //     const $ = cheerio.load(body);
+    //     console.log(
+    //       $("title").text(),
+    //       // $.html(),
+    //       // response
+    //     )
+    //   });
+
+    // faf()
+    // let res = getUser()
+
+    // request('https://www.mangaread.org/manga/overgeared-2020-manhwa/chapter-115/',
+    //   {
+    //     method: 'GET',
+    //     followRedirect: false,
+    //     followAllRedirects: false
+    //   },
+    //   function (error, response, body) {
+    //     // console.error('error:', error); // Print the error if one occurred
+    //     console.log('1115 statusCode:', response.statusCode); // Print the response status code if a response was received
+    //     // console.log('statusCode:', response); // Print the response status code if a response was received
+    //     // console.log('body:', body); // Print the HTML for the Google homepage.
+    //     // const cheerio = require('cheerio');
+    //     const $ = cheerio.load(body);
+    //     console.log(
+    //       $("title").text()
+    //     )
+    //   });
 
 
 
-    request('https://www.mangaread.org/manga/overgeared-2020-manhwa/chapter-115/',
-      {
-        method: 'GET',
-        followRedirect: false,
-        followAllRedirects: false
-      },
-      function (error, response, body) {
-        // console.error('error:', error); // Print the error if one occurred
-        console.log('1115 statusCode:', response.statusCode); // Print the response status code if a response was received
-        // console.log('statusCode:', response); // Print the response status code if a response was received
-        // console.log('body:', body); // Print the HTML for the Google homepage.
-        // const cheerio = require('cheerio');
-        const $ = cheerio.load(body);
-        console.log(
-          $("title").text()
-        )
-      });
 
-
-
+    // Make a request for a user with a given ID
+    // axios.get('https://w12.mangafreak.net/Read1_Onepunch_Man_156')
+    //   .then(function (response) {
+    //     // handle success
+    //     // console.log(response);
+    //     const $ = cheerio.load(response.data);
+    //     console.log(
+    //       $("title").text(), $("title").text().toLocaleLowerCase()
+    //     )
+    //     siteStatusResponse.result = true
+    //   })
+    //   .catch(function (error) {
+    //     // handle error
+    //     console.log("ERR", error);
+    //   })
 
     // request('https://w12.mangafreak.net/Read1_Onepunch_Man_156',
-    request('https://www.mangaread.org/manga/the-beginning-after-the-end/chapter-130/',
-      {
-        method: 'GET',
-        followRedirect: false,
-        followAllRedirects: false
-      },
-      function (error, response, body) {
-        // console.error('error:', error); // Print the error if one occurred
-        console.log('ok statusCode:', response.statusCode); // Print the response status code if a response was received
-        // console.log('statusCode:', response); // Print the response status code if a response was received
-        // console.log('body:', body); // Print the HTML for the Google homepage.
-        const $ = cheerio.load(body);
-        console.log(
-          $("title").text()
-        )
-      });
+    //   // request('https://www.mangaread.org/manga/the-beginning-after-the-end/chapter-130/',
+    //   {
+    //     method: 'GET',
+    //     followRedirect: false,
+    //     followAllRedirects: false
+    //   },
+    //   function (error, response, body) {
+    //     // console.error('error:', error); // Print the error if one occurred
+    //     console.log('ok statusCode:', response.statusCode); // Print the response status code if a response was received
+    //     // console.log('statusCode:', response); // Print the response status code if a response was received
+    //     // console.log('body:', body); // Print the HTML for the Google homepage.
+    //     const $ = cheerio.load(body);
+    //     console.log(
+    //       $("title").text(), $("title").text().toLocaleLowerCase()
+    //     )
+    //     siteStatusResponse.result = $("title").text().toLocaleLowerCase().includes("chapter")
+    //     console.log(
+    //       siteStatusResponse
+    //     )
+    //     // return siteStatusResponse
+    //     return new HttpResponseOK(siteStatusResponse);
+    //   });
 
 
-    // request('https://w12.mangafreak.net/Read1_Onepunch_Man_157',
-    request('https://www.mangaread.org/manga/the-beginning-after-the-end/chapter-128/',
+    // // request('https://w12.mangafreak.net/Read1_Onepunch_Man_157',
+    // request('https://www.mangaread.org/manga/the-beginning-after-the-end/chapter-128/',
+    //   {
+    //     method: 'GET',
+    //     followRedirect: false,
+    //     followAllRedirects: false
+    //   },
+    //   function (error, response, body) {
+    //     // console.error('error:', error); // Print the error if one occurred
+    //     console.log('mo statusCode:', response.statusCode); // Print the response status code if a response was received
+    //     // console.log('statusCode:', response); // Print the response status code if a response was received
+    //     // console.log('body:', body); // Print the HTML for the Google homepage.
+    //     const $ = cheerio.load(body);
+    //     console.log(
+    //       $("title").text()
+    //     )
+    //   });
+    // console.log(res)
+    // let resData = true
+    // res.then(data => {
+    //   // response.json({ message: 'Request received!', data })
+    //   console.log(data)
+    //   resData = data || false
+    //   // return new HttpResponseOK(resData);
+    // })
+    // const axios = require('axios');
+    // Axios.axio
+    const res = await axios.get('https://w12.mangafreak.net/Read1_Onepunch_Man_156');
+    const $ = cheerio.load(res.data);
+    // console.log(
+    //   $("title").text()
+    // )
+
+    // siteStatusResponse.result = $("title").text().toLocaleLowerCase().includes("chapter")
+
+    return new HttpResponseOK(
+      // $("title").text()
+      // $("title").text().toLocaleLowerCase().includes("chapter")
+      // siteStatusResponse
       {
-        method: 'GET',
-        followRedirect: false,
-        followAllRedirects: false
-      },
-      function (error, response, body) {
-        // console.error('error:', error); // Print the error if one occurred
-        console.log('mo statusCode:', response.statusCode); // Print the response status code if a response was received
-        // console.log('statusCode:', response); // Print the response status code if a response was received
-        // console.log('body:', body); // Print the HTML for the Google homepage.
-        const $ = cheerio.load(body);
-        console.log(
-          $("title").text()
-        )
-      });
-    return new HttpResponseOK();
+        result: $("title").text().toLocaleLowerCase().includes("chapter")
+      }
+    );
+    return new HttpResponseOK(await axios.get('https://w12.mangafreak.net/Read1_Onepunch_Man_156').then(
+      p => { return p }
+    ));
+    // return axios.get('https://w12.mangafreak.net/Read1_Onepunch_Man_156');
   }
 }
 
+async function faf() {
+  siteStatusResponse.result = true
+}
+
+// async function getUser() {
+//   try {
+//     siteStatusResponse.result = true
+//     const response = await axios.get('https://w12.mangafreak.net/Read1_Onepunch_Man_156');
+//     // siteStatusResponse.result = false
+//     // console.log(response);
+//     return false
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+function getUser() {
+  try {
+    siteStatusResponse.result = true
+    const response = axios.get('https://w12.mangafreak.net/Read1_Onepunch_Man_156');
+    // siteStatusResponse.result = false
+    console.log(response);
+    // const $ = cheerio.load(response.data);
+    // console.log(
+    //   $("title").text(), $("title").text().toLocaleLowerCase()
+    // )
+    return [false, false]
+  } catch (error) {
+    console.error(error);
+  }
+}
